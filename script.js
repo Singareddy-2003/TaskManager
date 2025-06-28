59b80b6a58601f7319b2cb64d9f4f38cea1f6c77
Singareddy-2003 <mahanthsingareddy3@gmail.com>
2025-06-28 11:28:57 +0530

    completed task manager pro

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let u_name = document.querySelector("#susername")
+let u_email = document.querySelector("#semail")
+let u_password = document.querySelector("#spassword")
+
+// function for the sign up
+
+document.querySelector("#signupBtn").addEventListener("click",function () {
+    if(u_name && u_email && u_password){
+             let details = {username : u_name.value ,email:u_email.value , password : u_password.value }
+             window.localStorage.setItem("details",JSON.stringify(details))
+             window.location.href="login.html"
+    }
+    else{
+        alert("plese enter all credentials ")
+    }
+})
+
+
+
